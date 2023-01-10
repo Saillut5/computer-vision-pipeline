b957b44db62d46c609906d4121254c01b64b4d35
Silas Vance <phosubvieworsi@gmx.net>
2023-01-10 13:23:00 -0500

    refactor: Modularize model architectures

---
+++ cv_pipeline/utils/__init__.py
@@ -0,0 +1,10 @@
+# cv_pipeline/utils/__init__.py
+
+from .transforms import get_default_transforms, get_augmentation_transforms, get_inference_transforms
+
+__all__ = [
+    "get_default_transforms",
+    "get_augmentation_transforms",
+    "get_inference_transforms",
+]
+# Simulated change on 2023-01-10 13:23:00
