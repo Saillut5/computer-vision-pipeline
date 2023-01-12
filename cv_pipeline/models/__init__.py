d9df277f3eb3bebbf45f4afad5fb17e6932e71d7
Silas Vance <phosubvieworsi@gmx.net>
2023-01-12 14:28:00 -0500

    refactor: Extract common CV utilities to a shared module

---
+++ cv_pipeline/models/__init__.py
@@ -0,0 +1,10 @@
+# cv_pipeline/models/__init__.py
+
+from .resnet import ResNetClassifier
+from .efficientnet import EfficientNetClassifier
+
+__all__ = [
+    "ResNetClassifier",
+    "EfficientNetClassifier",
+]
+# Simulated change on 2023-01-12 14:28:00
