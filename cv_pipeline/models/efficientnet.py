0ed34cc5bb114adda075741442cbcbcdfdfe5a55
Silas Vance <phosubvieworsi@gmx.net>
2023-01-18 13:54:00 -0500

    perf: Optimize data loading with multiprocessing

--- cv_pipeline/models/efficientnet.py
+++ cv_pipeline/models/efficientnet.py
@@ -58,3 +58,4 @@ if __name__ == '__main__':
     # Expecting most of the feature extractor parameters to be frozen
     assert frozen_params > 50, "Feature extractor parameters not frozen as expected."
 # Simulated change on 2023-01-05 13:54:00
+# Simulated change on 2023-01-18 13:54:00
