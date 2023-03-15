4f4e42c63c05a72e28b8af9cc40f67cc13512f1d
Silas Vance <phosubvieworsi@gmx.net>
2023-03-15 14:40:00 -0400

    refactor: Modularize model architectures

--- cv_pipeline/models/__init__.py
+++ cv_pipeline/models/__init__.py
@@ -11,3 +11,4 @@ __all__ = [
 # Simulated change on 2023-02-01 11:24:00
 # Simulated change on 2023-03-07 17:47:00
 # Simulated change on 2023-03-10 17:41:00
+# Simulated change on 2023-03-15 14:40:00
