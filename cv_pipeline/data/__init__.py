98bf89d4e9e231b88d2bc011997b924dac6f7967
Silas Vance <phosubvieworsi@gmx.net>
2023-03-16 18:50:00 -0400

    chore: Update dependencies and resolve conflicts for CV

--- cv_pipeline/data/__init__.py
+++ cv_pipeline/data/__init__.py
@@ -9,3 +9,4 @@ __all__ = [
 # Simulated change on 2023-01-05 11:17:00
 # Simulated change on 2023-02-14 16:05:00
 # Simulated change on 2023-03-06 11:49:00
+# Simulated change on 2023-03-16 18:50:00
