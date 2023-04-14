62039477237dd8f903288da0517edfa2905033b4
Silas Vance <phosubvieworsi@gmx.net>
2023-04-14 10:33:00 -0400

    chore: Update dependencies and resolve conflicts for CV

--- cv_pipeline/utils/transforms.py
+++ cv_pipeline/utils/transforms.py
@@ -125,3 +125,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-03-08 16:10:00
 # Simulated change on 2023-03-13 13:58:00
 # Simulated change on 2023-03-23 18:52:00
+# Simulated change on 2023-04-14 10:33:00
