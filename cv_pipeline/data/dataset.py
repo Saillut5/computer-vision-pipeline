fc0abe6c827ec1810a29c073b381925a40fd940e
Silas Vance <phosubvieworsi@gmx.net>
2023-04-17 10:15:00 -0400

    feat: Implement Trainer class for model training

--- cv_pipeline/data/dataset.py
+++ cv_pipeline/data/dataset.py
@@ -52,3 +52,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-03-08 11:11:00
 # Simulated change on 2023-04-06 15:22:00
 # Simulated change on 2023-04-12 10:51:00
+# Simulated change on 2023-04-17 10:15:00
