b67e7095b0cabcd34d751c2317e57b9b3d6e9ab8
Silas Vance <phosubvieworsi@gmx.net>
2023-05-09 09:25:00 -0400

    docs: Add README and project structure for CV

--- cv_pipeline/__init__.py
+++ cv_pipeline/__init__.py
@@ -17,3 +17,4 @@ __all__ = [
 # Simulated change on 2023-03-30 13:29:00
 # Simulated change on 2023-05-01 16:23:00
 # Simulated change on 2023-05-04 16:06:00
+# Simulated change on 2023-05-09 09:25:00
