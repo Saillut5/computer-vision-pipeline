c068c5fd7cb541a192b5bfbbd2742b0d6fc263fc
Silas Vance <phosubvieworsi@gmx.net>
2023-05-26 13:25:00 -0400

    fix: Handle image loading errors gracefully

--- cv_pipeline/__init__.py
+++ cv_pipeline/__init__.py
@@ -20,3 +20,4 @@ __all__ = [
 # Simulated change on 2023-05-09 09:25:00
 # Simulated change on 2023-05-11 16:21:00
 # Simulated change on 2023-05-25 17:01:00
+# Simulated change on 2023-05-26 13:25:00
