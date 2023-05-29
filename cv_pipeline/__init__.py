74cfd9825dc9bfc3e1db65372118f1c52d5e4f8d
Silas Vance <phosubvieworsi@gmx.net>
2023-05-29 18:05:00 -0400

    feat: Integrate EfficientNet model

--- cv_pipeline/__init__.py
+++ cv_pipeline/__init__.py
@@ -21,3 +21,4 @@ __all__ = [
 # Simulated change on 2023-05-11 16:21:00
 # Simulated change on 2023-05-25 17:01:00
 # Simulated change on 2023-05-26 13:25:00
+# Simulated change on 2023-05-29 18:05:00
