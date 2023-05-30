bc630cb0af38262ac79bd5e7dfcdd726a1c43bb6
Silas Vance <phosubvieworsi@gmx.net>
2023-05-30 12:09:00 -0400

    refactor: Clean up model definitions and layers

--- cv_pipeline/models/resnet.py
+++ cv_pipeline/models/resnet.py
@@ -291,3 +291,4 @@ if __name__ == \'__main__\':
 # Simulated change on 2023-04-21 17:26:00
 # Simulated change on 2023-05-02 12:04:00
 # Simulated change on 2023-05-24 15:04:00
+# Simulated change on 2023-05-30 12:09:00
