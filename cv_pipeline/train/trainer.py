98f3f789d5e2924be7680daee71ed72922490127
Silas Vance <phosubvieworsi@gmx.net>
2023-06-21 16:30:00 -0400

    perf: Optimize data loading with multiprocessing

--- cv_pipeline/train/trainer.py
+++ cv_pipeline/train/trainer.py
@@ -122,3 +122,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-05-26 10:59:00
 # Simulated change on 2023-05-30 11:45:00
 # Simulated change on 2023-06-14 13:51:00
+# Simulated change on 2023-06-21 16:30:00
