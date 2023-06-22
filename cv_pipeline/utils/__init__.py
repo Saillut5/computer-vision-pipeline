43300b2ed50cdb9038f231cbd04962a3d8c296de
Silas Vance <phosubvieworsi@gmx.net>
2023-06-22 16:36:00 -0400

    fix: Address minor bugs in training loop

--- cv_pipeline/utils/__init__.py
+++ cv_pipeline/utils/__init__.py
@@ -13,3 +13,4 @@ __all__ = [
 # Simulated change on 2023-02-09 13:34:00
 # Simulated change on 2023-03-02 11:01:00
 # Simulated change on 2023-05-25 09:42:00
+# Simulated change on 2023-06-22 16:36:00
