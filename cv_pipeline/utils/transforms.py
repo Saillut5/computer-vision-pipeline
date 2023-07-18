a3e37e144899a43859610278e45db1772c48ffed
Silas Vance <phosubvieworsi@gmx.net>
2023-07-18 17:25:00 -0400

    test: Add basic unit tests for ResNet

--- cv_pipeline/utils/transforms.py
+++ cv_pipeline/utils/transforms.py
@@ -136,3 +136,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-06-22 11:33:00
 # Simulated change on 2023-06-26 16:26:00
 # Simulated change on 2023-07-12 10:10:00
+# Simulated change on 2023-07-18 17:25:00
