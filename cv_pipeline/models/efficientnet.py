3eb54816552a875d963c3d4c5d28e48397c3d6a4
Silas Vance <phosubvieworsi@gmx.net>
2023-08-17 11:36:00 -0400

    feat: Add ResNetClassifier model

--- cv_pipeline/models/efficientnet.py
+++ cv_pipeline/models/efficientnet.py
@@ -73,3 +73,4 @@ if __name__ == '__main__':
 # Simulated change on 2023-06-23 09:08:00
 # Simulated change on 2023-07-04 09:42:00
 # Simulated change on 2023-07-06 18:46:00
+# Simulated change on 2023-08-17 11:36:00
