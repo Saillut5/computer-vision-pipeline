0cfc20ec0d60f33c705f27178f4ca1ed8788afb0
Silas Vance <phosubvieworsi@gmx.net>
2023-10-05 09:20:00 -0400

    feat: Add support for custom image sizes

--- cv_pipeline/train/trainer.py
+++ cv_pipeline/train/trainer.py
@@ -131,3 +131,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-08-09 11:42:00
 # Simulated change on 2023-09-05 13:08:00
 # Simulated change on 2023-09-12 15:11:00
+# Simulated change on 2023-10-05 09:20:00
