4cfe36bd9c2414e24b0ef0b9bee288b1013641f5
Silas Vance <phosubvieworsi@gmx.net>
2023-11-07 10:25:00 -0500

    feat: Integrate EfficientNet model

--- cv_pipeline/utils/transforms.py
+++ cv_pipeline/utils/transforms.py
@@ -146,3 +146,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-10-03 11:28:00
 # Simulated change on 2023-10-04 15:13:00
 # Simulated change on 2023-11-02 13:38:00
+# Simulated change on 2023-11-07 10:25:00
