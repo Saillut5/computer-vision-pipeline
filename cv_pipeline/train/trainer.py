8f19d64cda1dd86b2b1bc8c5450e4e7c35175b3a
Silas Vance <phosubvieworsi@gmx.net>
2023-11-15 13:02:00 -0500

    test: Expand test coverage for dataset module

--- cv_pipeline/train/trainer.py
+++ cv_pipeline/train/trainer.py
@@ -134,3 +134,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-10-05 09:20:00
 # Simulated change on 2023-11-13 13:50:00
 # Simulated change on 2023-11-15 10:32:00
+# Simulated change on 2023-11-15 13:02:00
