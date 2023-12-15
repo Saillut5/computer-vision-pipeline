5a741787f5ef22fb9b4ef1e37bdf56d99a66041e
Silas Vance <phosubvieworsi@gmx.net>
2023-12-15 18:30:00 -0500

    refactor: Modularize model architectures

--- cv_pipeline/models/resnet.py
+++ cv_pipeline/models/resnet.py
@@ -308,3 +308,4 @@ if __name__ == \'__main__\':
 # Simulated change on 2023-11-21 16:51:00
 # Simulated change on 2023-11-28 14:10:00
 # Simulated change on 2023-11-29 10:15:00
+# Simulated change on 2023-12-15 18:30:00
