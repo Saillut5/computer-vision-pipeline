8a269c39f1bdda4af7de46ef3a387da1a65dc588
Silas Vance <phosubvieworsi@gmx.net>
2024-01-01 16:34:00 -0500

    feat: Add ResNetClassifier model

--- cv_pipeline/__init__.py
+++ cv_pipeline/__init__.py
@@ -40,3 +40,4 @@ __all__ = [
 # Simulated change on 2023-12-12 12:44:00
 # Simulated change on 2023-12-18 14:07:00
 # Simulated change on 2023-12-29 14:06:00
+# Simulated change on 2024-01-01 16:34:00
