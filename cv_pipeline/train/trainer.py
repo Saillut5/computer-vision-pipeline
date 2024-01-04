6ba11b76b815916b481fbd25c331092e175bed79
Silas Vance <phosubvieworsi@gmx.net>
2024-01-04 14:50:00 -0500

    test: Add basic unit tests for ResNet

--- cv_pipeline/train/trainer.py
+++ cv_pipeline/train/trainer.py
@@ -140,3 +140,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-11-27 17:31:00
 # Simulated change on 2023-11-29 12:46:00
 # Simulated change on 2023-12-22 18:38:00
+# Simulated change on 2024-01-04 14:50:00
