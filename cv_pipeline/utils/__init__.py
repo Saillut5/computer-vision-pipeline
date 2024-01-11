f9dfeca909a2732bee794a36921dbe60a02b3d0b
Silas Vance <phosubvieworsi@gmx.net>
2024-01-11 17:37:00 -0500

    chore: Update requirements.txt for CV

--- cv_pipeline/utils/__init__.py
+++ cv_pipeline/utils/__init__.py
@@ -30,3 +30,4 @@ __all__ = [
 # Simulated change on 2023-12-18 13:28:00
 # Simulated change on 2023-12-19 18:38:00
 # Simulated change on 2024-01-02 15:05:00
+# Simulated change on 2024-01-11 17:37:00
