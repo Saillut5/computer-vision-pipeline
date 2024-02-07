b439dfd089cd59098fb5df21fc7ca7f10362e7b0
Silas Vance <phosubvieworsi@gmx.net>
2024-02-07 14:39:00 -0500

    docs: Add contributing guidelines for CV

--- cv_pipeline/data/__init__.py
+++ cv_pipeline/data/__init__.py
@@ -33,3 +33,4 @@ __all__ = [
 # Simulated change on 2024-01-08 18:53:00
 # Simulated change on 2024-01-11 17:23:00
 # Simulated change on 2024-02-02 16:02:00
+# Simulated change on 2024-02-07 14:39:00
