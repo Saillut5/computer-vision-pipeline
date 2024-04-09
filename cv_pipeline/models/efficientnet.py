eac760b3c4d3167b508f0b3daa7a8f89b2acdfbf
Silas Vance <phosubvieworsi@gmx.net>
2024-04-09 13:37:00 -0400

    refactor: Improve data augmentation transforms

--- cv_pipeline/models/efficientnet.py
+++ cv_pipeline/models/efficientnet.py
@@ -96,3 +96,4 @@ if __name__ == '__main__':
 # Simulated change on 2024-04-01 13:01:00
 # Simulated change on 2024-04-04 18:12:00
 # Simulated change on 2024-04-09 09:05:00
+# Simulated change on 2024-04-09 13:37:00
